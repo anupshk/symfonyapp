02344e017476eabf999caacbcc0defd6283eb06d
anup <ashakya@braindigit.com>
2015-06-12 13:28:32 +0545

    user registration from admin, enhancements

--- src/Braindigit/UserBundle/Controller/UserController.php
+++ src/Braindigit/UserBundle/Controller/UserController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Braindigit\UserBundle\Controller;
 
+use Braindigit\UserBundle\Entity\User;
+use Braindigit\UserBundle\Form\Type\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -9,20 +11,12 @@ class UserController extends Controller
     public function indexAction(Request $request)
     {
         $list_data = [];
-        //$session = $this->get('session');
-
         $list_data['sort_field'] = 'id';
         $list_data['sort_direction'] = 'ASC';
-
         $list_data['rpp'] = 10;
+
         $rpp = $request->query->get('rpp', $list_data['rpp']);
         $search = $request->query->get('search','');
-        /*$userManager = $this->container->get('fos_user.user_manager');
-        $userManager->setDefaultOptions(array(
-            'sortField' => $request->query->getAlpha($this->container->getParameter('query_param_sort'), 'id'),
-            'sortDirection' => $request->query->getAlpha($this->container->getParameter('query_param_direction'), 'DESC'),
-        ));
-        $users = $userManager->findAllUsers();*/
 
         $criteria = [];
         if(mb_strlen($search) > 0) {
@@ -56,10 +50,6 @@ class UserController extends Controller
                 ]
             ];*/
         }
-        /*$orderings = [
-            ['field' => 'fullname', 'order' => 'ASC'],
-            ['field' => 'id', 'order' => 'DESC'],
-        ];*/
         $orderings = [
             [
                 'field' => $request->query->getAlpha($this->container->getParameter('query_param_sort'), 'id'),
@@ -71,7 +61,6 @@ class UserController extends Controller
         $repository = $this->getDoctrine()->getRepository('BraindigitUserBundle:User');
         $usersQB = $repository->findAllUsersQB($criteria, $orderings);
         $paginator  = $this->get('knp_paginator');
-        //$alias = current($usersQB->getDQLPart('from'))->getAlias().'.';
         $pagination = $paginator->paginate(
             $usersQB->getQuery(),
             $request->query->getInt('page', 1),
@@ -87,6 +76,28 @@ class UserController extends Controller
             'list_data' => $list_data,
             'rpp' => $rpp,
             'search' => $search
+        ));
+    }
+
+    public function formAction(Request $request)
+    {
+        $formFactory = $this->get('fos_user.registration.form.factory');
+        $userManager = $this->get('fos_user.user_manager');
+
+        $user = $userManager->createUser();
+        $user->setEnabled(true);
+
+        $form = $formFactory->createForm();
+        $form->setData($user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $userManager->updateUser($user);
+            $this->addFlash('success', 'User "'.$user->getUsername().'" saved!');
+            return $this->redirectToRoute('braindigit_user_list');
+        }
+        return $this->render('BraindigitUserBundle:User:form.html.twig', array(
+            'form' => $form->createView()
         ));
     }
 }
